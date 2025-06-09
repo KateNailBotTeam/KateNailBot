0e5f7f736aaaed890cfd413bca2f324f34d353ba
Win.freez <Win.freez@yandex.ru>
2025-06-09 13:30:36 +0300

    Добавлен middleware для подключения к БД

---
+++ src/middlewares/db_middleware.py
@@ -0,0 +1,20 @@
+from collections.abc import Awaitable, Callable
+from typing import Any
+
+from aiogram.middlewares import BaseMiddleware
+from aiogram.types.base import TelegramObject
+
+from database.database import get_session
+
+
+class DatabaseMiddleware(BaseMiddleware):
+    async def __call__(
+        self,
+        handler: Callable[[TelegramObject, dict], Awaitable[Any]],
+        event: TelegramObject,
+        data: dict,
+    ) -> Awaitable[Any]:
+        async for session in get_session():
+            data["session"] = session
+            return await handler(event, data)
+        return await handler(event, data)
