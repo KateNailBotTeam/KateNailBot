a2c5a696d19fa89587806f84d51278afb4aba985
Win.freez <Win.freez@yandex.ru>
2025-08-19 21:50:59 +0300

    Изменены настройки engine (sqlalchemy) для деплоя

--- database/database.py
+++ database/database.py
@@ -9,7 +9,12 @@ from sqlalchemy.ext.asyncio import (
 from src.config import settings
 
 engine = create_async_engine(
-    url=settings.db_url, echo=True, pool_size=50, max_overflow=10
+    url=settings.db_url,
+    echo=False,
+    pool_size=5,
+    max_overflow=10,
+    pool_timeout=30,
+    pool_recycle=1800,
 )
 session_factory = async_sessionmaker(
     bind=engine, class_=AsyncSession, expire_on_commit=False
